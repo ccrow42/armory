ec9a627f45f6221cca311667b54d4ea5bc9ad484
ccrow42 <chris@ccrow.org>
2023-05-18 11:45:26 -0700

    update

--- bbq-taster/bbq-taster.py
+++ bbq-taster/bbq-taster.py
@@ -1,14 +1,41 @@
 from flask import Flask
-from pymongo import MongoClient
+import pymongo
+import urllib.parse
+import os
+
+#username = urllib.parse.quote_plus('porxie')
+#password = urllib.parse.quote_plus('porxie')
+
+username = os.getenv('MONGO_USER')
+password = os.getenv('MONGO_PASS')
+server = os.getenv('MONGO_NODE')
 
 app = Flask(__name__)
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+def get_document_count(database_name, collection_name):
+    # Create a connection to the MongoDB instance running on localhost
+    client = pymongo.MongoClient("mongodb://%s:%s@%s:27017/" % (username, password, server)) 
+
+    # Access the specific database
+    db = client[database_name]
+
+    # Access the specific collection
+    collection = db[collection_name]
+
+    # Use the count_documents() method to get the number of documents
+    document_count = collection.count_documents({})
+
+    return document_count
+
+# Use the function
+docCount = get_document_count("porxbbq", "orders")
+
 
 @app.route('/')
 def hello_geek():
-    return (myclient.list_database_names())
+    return ("%s orders served from PorxBBQ" % docCount)
 
+print ("%s orders served from PorxBBQ" % docCount)
+app.run(host='0.0.0.0')
 
-if __name__ == "__main__":
-    app.run(debug=True)
